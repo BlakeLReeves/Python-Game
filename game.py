efc68520354b351f07532b6bbf3f23d793f549eb
blakereeves19 <blake.reeves19@gmail.com>
2019-06-30 18:45:28 -0500

    Added changes

--- game.py
+++ game.py
@@ -5,29 +5,85 @@
 
 # player = ['Manuel', 10, 16, 100]
 
-player = {'name': 'Manuel', 'attack': 10, 'heal': 16, 'health': 100}
-monster = {'name': 'Max', 'attack': 12, 'health': 100}
 game_running = True
 
 while game_running == True:
+    
+    new_round = True
+    player = {'name': 'Manuel', 'attack': 10, 'heal': 16, 'health': 100}
+    monster = {'name': 'Max', 'attack': 12, 'health': 100}
 
-    print('Please select action')
-    print('1) Attack')
-    print('2) Heal')
+    print('---' * 7)
+    print('Enter Player Name:')
+    print('---' * 7)
+    player['name'] = input()
 
-    player_choice = input()
-    player_health_lbl = 'Player Health: '
-    monster_health_lbl = 'Monster Health: '
+    print(player['name'])
 
-    if player_choice == 1:
-        monster['health'] = monster['health'] - player['attack']
-        player['health'] = player['health'] - monster['attack']
-        print(monster_health_lbl + str(monster['health']))
-        print(player_health_lbl + str(player['health']))
-    elif player_choice == 2:
-        print('Heal Player')
-    else:
-        print('Invalid Input')
+    while new_round == True:
 
-    if player['health'] | monster['health'] <= 0:
-        game_running = False
+
+        player_won = False
+        monster_won = False
+
+        print('---' * 7)
+        print('Please select action')
+        print('---' * 7)
+        print('1) Attack')
+        print('2) Heal')
+        print('3) Exit Game')
+        print('---' * 7)
+
+        player_choice = input()
+        player_health_lbl = player['name'] + "'s" + ' Health: '
+        monster_health_lbl = monster['name'] + "'s" + ' Health: '
+
+        if player_choice == 1:
+
+            monster['health'] = monster['health'] - player['attack']
+            if monster['health'] <= 0:
+                player_won = True
+
+            else:
+
+                player['health'] = player['health'] - monster['attack']
+                if player['health'] <= 0:
+                    monster_won = True
+
+            print(monster_health_lbl + str(monster['health']))
+            print(player_health_lbl + str(player['health']))
+
+        elif player_choice == 2:
+
+            player['health'] = player['health'] + player['heal']
+            print(player_health_lbl + str(player['health']))
+
+        elif player_choice == 3:
+
+            print('---' * 7)
+            print('Exited Game')
+            print('---' * 7)
+
+            print('Results:')
+            print('---' * 7)
+            print(monster_health_lbl + str(monster['health']))
+            print(player_health_lbl + str(player['health']))
+            print('---' * 7)
+
+            new_round = False
+            game_running = False
+
+        else:
+
+            print('Invalid Input')
+
+        if player_won == True or monster_won == True:
+            
+            print('---' * 7)
+            print('Results:')
+            print('---' * 7)
+            print(monster_health_lbl + str(monster['health']))
+            print(player_health_lbl + str(player['health']))
+            print('---' * 7)
+
+            new_round = False
